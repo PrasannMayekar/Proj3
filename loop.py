01350259514d1154f56a09c25c5e7cd308b0d003
PrasannMayekar <prasannm@gmail.com>
2023-12-14 16:14:47 +0530

    big 1 fixed

--- loop.py
+++ loop.py
@@ -5,14 +5,19 @@ while(n<5):
  print("n= ", n)
  n=n+1
 print("lop over") '''
-honor_roll_count = 0
+honor_roll_count = 100
 student_grades = ["A", "C", "B", "B", "C", "A", "F", "B", "B", "B", "C", "A"]
 for grade in student_grades:
     if grade == "A" or grade == "B":
-        honor_roll_count = honor_roll_count + 2
+        honor_roll_count = honor_roll_count + 1
         
 print (honor_roll_count)
 print (honor_roll_count)
+print (honor_roll_count)
+print (honor_roll_count)
+
+
+
 
 
 
